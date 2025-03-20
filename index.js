dc75d784e1cb962915470d1c26341ab9e5312997
nzantunes <nzantunes@users.noreply.github.com>
2025-03-20 18:07:31 -0300

    fix: ajusta configuração do puppeteer para VPS

--- index.js
+++ index.js
@@ -376,9 +376,19 @@ function createOpenAIClientLocal() {
 
 // Configuração do cliente WhatsApp
 const client = new Client({
+    authStrategy: new LocalAuth(),
     puppeteer: {
         headless: true,
-        args: ['--no-sandbox']
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-accelerated-2d-canvas',
+            '--no-first-run',
+            '--no-zygote',
+            '--disable-gpu'
+        ],
+        executablePath: '/usr/bin/chromium-browser'
     }
 });
 
