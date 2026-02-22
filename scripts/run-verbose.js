f2fcc5672424eb7a5899cd192c46239f98b3269b
nzantunes <116675396+nzantunes@users.noreply.github.com>
2026-02-22 03:35:43 -0300

    Delete scripts directory

--- scripts/run-verbose.js
+++
@@ -1,8 +0,0 @@
-/**
- * Inicia o servidor com logs detalhados (VERBOSE=1).
- * Uso: npm run start:verbose  ou  node scripts/run-verbose.js
- */
-process.env.VERBOSE = '1';
-const path = require('path');
-process.chdir(path.join(__dirname, '..'));
-require(path.join(__dirname, '..', 'src', 'server.js'));
